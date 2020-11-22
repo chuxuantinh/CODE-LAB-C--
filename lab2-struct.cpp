0d0f40e4c2dd89d3062eb91d7df3a9436b6e7b74
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2020-11-22 17:27:42 +0700

    Update lab2-struct.cpp

--- lab2-struct.cpp
+++ lab2-struct.cpp
@@ -6,10 +6,23 @@ struct toado{
 } diem;
 
 
-void Nhaptoado(toado N)
+void Nhaptoado(diem N)
 {
     cout<<"Nhap toa do %c";cin>>N;
     cout<<"Nhap toa do x:"; cin>>N.x;
     cout<<"Nhao toa do y:"; cin>>N.y;
 }
 
+double Tinh_Khoang_Cach(diem X, diem Y)
+{
+    double kc;
+    kc = sqrt(pow((X.x-Y.x),2)+ pow((X.y-Y.y),2));
+    return kc;
+}
+
+int main()
+{
+
+
+    
+}
