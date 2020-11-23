c1fe2b337b4d7dc954cb4f10d0de1fae1bcbc8d1
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2020-11-23 22:24:13 +0700

    Update Chuong2-1.cpp

--- Chuong2-1.cpp
+++ Chuong2-1.cpp
@@ -13,8 +13,26 @@ void In_binh_phuong()
         n++;
     }
 }
+
+//In ra n con số, mỗi con số trên 1 hàng ( 1+1, 1+2, 1+2+3)
+
+void In_Xuong_Hang()
+{
+    int n,tong =0;
+    cout<<"Nhap vao so n:" ; cin>>n;
+    for(int i =0; i<=n; i++)
+    {
+        tong = tong + i;
+        cout<<tong<<endl;
+    }
+    
+
+
+}
+
 int main()
 
 {
-    In_binh_phuong();
+    //In_binh_phuong();
+    In_Xuong_Hang();
 }
