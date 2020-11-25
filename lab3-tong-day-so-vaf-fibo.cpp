803e2f32d2f2b0cf58fcf9240585a5866d5535a1
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2020-11-25 14:36:12 +0700

    Update lab3-tong-day-so-vaf-fibo.cpp

--- lab3-tong-day-so-vaf-fibo.cpp
+++ lab3-tong-day-so-vaf-fibo.cpp
@@ -2,6 +2,20 @@
 #include <iostream>
 using namespace std;
 
+
+int fibonaci_t(int n)
+{
+    if (n<1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonaci_t(n-1) + fibonaci_t(n-2);
+    }
+    
+}
+
 int main()
 {
     int n;
@@ -12,5 +26,7 @@ int main()
     {
         tong = tong + i*i;
     }
-    cout<< "Tong binh phuong cac  so le la:" << tong;
+    cout<< "Tong binh phuong cac  so le la:" << tong <<endl;
+    int m = 1;
+    cout<<fibonaci_t(m);
 }
